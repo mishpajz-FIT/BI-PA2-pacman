2b89aba202c518374426993b94885024dea2ad94
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-03-05 14:24:57 +0100

    hw01: Implement naive pre-order binary tree rebuild

--- tasks/homework/hw01/huffmanuvkod.cpp
+++ tasks/homework/hw01/huffmanuvkod.cpp
@@ -21,74 +21,150 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
-bool decompressFile ( const char * inFileName, const char * outFileName )
-{
-  // todo
-  return false;
+struct Node {
+	char data;
+	Node * left;
+	Node * right;
+
+	Node(char newData = 0) {
+		data = newData;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+void insertNode(Node * at, queue<char> & positions, queue<char> & characters) {
+	if (positions.front() == 0) {
+		at->left = new Node;
+		positions.pop();
+		insertNode(at->left, positions, characters);
+	} else {
+		at->left = new Node(characters.front());
+		positions.pop();
+		characters.pop();
+	}
+
+	if (positions.front() == 0) {
+		at->right = new Node;
+		positions.pop();
+		insertNode(at->right, positions, characters);
+	} else {
+		at->right = new Node(characters.front());
+		positions.pop();
+		characters.pop();
+	}
 }
 
-bool compressFile ( const char * inFileName, const char * outFileName )
-{
-  // keep this dummy implementation (no bonus) or implement the compression (bonus)
-  return false;
+void printTree(Node * from) {
+
+	if (from->right != nullptr) {
+		printTree(from->right);
+	}
+
+	if (from->left != nullptr) {
+		printTree(from->left);
+	}
+
+	if (from->data != 0) {
+		cout << from->data;
+	}
 }
+
+void test() {
+	Node * root = new Node;
+	queue<char> positions;
+	queue<char> characters;
+
+	positions.push(1);
+	positions.push(0);
+	positions.push(0);
+	positions.push(1);
+	positions.push(1);
+	positions.push(0);
+	positions.push(1);
+	positions.push(1);
+
+	characters.push('o');
+	characters.push('t');
+	characters.push('c');
+	characters.push('K');
+	characters.push('l');
+
+	insertNode(root, positions, characters);
+
+	printTree(root);
+	cout << endl;
+}
+
+bool decompressFile(const char * inFileName, const char * outFileName) {
+	// todo
+	return false;
+}
+
+bool compressFile(const char * inFileName, const char * outFileName) {
+  	// keep this dummy implementation (no bonus) or implement the compression (bonus)
+	return false;
+}
+
 #ifndef __PROGTEST__
-bool identicalFiles ( const char * fileName1, const char * fileName2 )
-{
-  // todo
-  return false;
+bool identicalFiles(const char * fileName1, const char * fileName2) {
+	// todo
+	return false;
 }
 
-int main ( void )
-{
-  assert ( decompressFile ( "tests/test0.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/test0.orig", "tempfile" ) );
+int main(void) {
 
-  assert ( decompressFile ( "tests/test1.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/test1.orig", "tempfile" ) );
+	test();
 
-  assert ( decompressFile ( "tests/test2.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/test2.orig", "tempfile" ) );
+	/*
+	assert(decompressFile("tests/test0.huf", "tempfile"));
+	assert(identicalFiles("tests/test0.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/test3.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/test3.orig", "tempfile" ) );
+	assert(decompressFile("tests/test1.huf", "tempfile"));
+	assert(identicalFiles("tests/test1.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/test4.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/test4.orig", "tempfile" ) );
+	assert(decompressFile("tests/test2.huf", "tempfile"));
+	assert(identicalFiles("tests/test2.orig", "tempfile"));
 
-  assert ( ! decompressFile ( "tests/test5.huf", "tempfile" ) );
+	assert(decompressFile("tests/test3.huf", "tempfile"));
+	assert(identicalFiles("tests/test3.orig", "tempfile"));
+
+	assert(decompressFile("tests/test4.huf", "tempfile"));
+	assert(identicalFiles("tests/test4.orig", "tempfile"));
+
+	assert(!decompressFile("tests/test5.huf", "tempfile"));
 
 
-  assert ( decompressFile ( "tests/extra0.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra0.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra0.huf", "tempfile"));
+	assert(identicalFiles("tests/extra0.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra1.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra1.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra1.huf", "tempfile"));
+	assert(identicalFiles("tests/extra1.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra2.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra2.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra2.huf", "tempfile"));
+	assert(identicalFiles("tests/extra2.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra3.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra3.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra3.huf", "tempfile"));
+	assert(identicalFiles("tests/extra3.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra4.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra4.orig", "tempfile" ) );
+	assert(decompressFile ( "tests/extra4.huf", "tempfile" ) );
+	assert(identicalFiles ( "tests/extra4.orig", "tempfile" ) );
 
-  assert ( decompressFile ( "tests/extra5.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra5.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra5.huf", "tempfile"));
+	assert(identicalFiles("tests/extra5.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra6.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra6.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra6.huf", "tempfile"));
+	assert(identicalFiles("tests/extra6.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra7.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra7.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra7.huf", "tempfile"));
+	assert(identicalFiles("tests/extra7.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra8.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra8.orig", "tempfile" ) );
+	assert(decompressFile("tests/extra8.huf", "tempfile"));
+	assert(identicalFiles("tests/extra8.orig", "tempfile"));
 
-  assert ( decompressFile ( "tests/extra9.huf", "tempfile" ) );
-  assert ( identicalFiles ( "tests/extra9.orig", "tempfile" ) );
-
-  return 0;
+	assert(decompressFile("tests/extra9.huf", "tempfile"));
+	assert(identicalFiles("tests/extra9.orig", "tempfile"));
+	return 0;
+	*/
 }
 #endif /* __PROGTEST__ */
