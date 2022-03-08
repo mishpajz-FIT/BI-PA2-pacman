62e8fe1ee15fd334b871665f2f768bde59e6943d
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-03-08 19:15:00 +0100

    ex04: Implement classes methods

--- tasks/exercise/ex04/erouska.cpp
+++ tasks/exercise/ex04/erouska.cpp
@@ -28,6 +28,20 @@ public:
     bool operator == (CDate & rhs) const {
         return isEqual(rhs);
     }
+
+    bool operator > (CDate & rhs) const {
+        
+        if (year == rhs.year) {
+            if (month == rhs.month) {
+                if (day == rhs.day) {
+                    return false;
+                }
+                return day > rhs.day;
+            }
+            return month > rhs.month;
+        }
+        return year > rhs.year;
+    }
 };
 
 class CTime {
@@ -45,6 +59,20 @@ public:
     bool operator == (CTime & rhs) const {
         return isEqual(rhs);
     }
+
+    bool operator > (CTime& rhs) const {
+
+        if (hour == rhs.hour) {
+            if (minute == rhs.minute) {
+                if (second == rhs.second) {
+                    return false;
+                }
+                return second > rhs.second;
+            }
+            return minute > rhs.minute;
+        }
+        return hour > rhs.hour;
+    }
 };
 
 class CTimeStamp {
@@ -61,24 +89,95 @@ public:
     bool operator == (CTimeStamp & rhs) const {
         return isEqual(rhs);
     }
+
+    bool operator >= (CTimeStamp & rhs) const {
+
+        if (date == rhs.date) {
+            if (time == rhs.time) {
+                return true;
+            }
+            return time > rhs.time;
+        }
+        return date > rhs.date;
+    }
+
+    bool operator >= (const CTimeStamp & rhs) const {
+        return (*this) >= const_cast<CTimeStamp&>(rhs);
+    }
 };
 
 class CContact {
     CTimeStamp timestamp;
-    const unsigned long int person1;
-    const unsigned long int person2;
+    const unsigned long person1;
+    const unsigned long person2;
+
+    static size_t nextUniqueId;
 
 public:
-    CContact(const CTimeStamp & ts, unsigned long p1, unsigned long p2): timestamp(ts), person1(p1), person2(p2) {}
+    CContact(const CTimeStamp& ts, unsigned long p1, unsigned long p2) : timestamp(ts), person1(p1), person2(p2) {}
+
+    bool containsPerson(unsigned int p) const {
+        return ((person1 == p) || (person2 == p));
+    }
+
+    bool containsPerson(unsigned int p, const CTimeStamp& fromT, const CTimeStamp& toT) {
+        return (containsPerson(p) && (timestamp >= fromT && toT >= timestamp));
+    }
+
+    unsigned long otherPerson(unsigned int to) const {
+        if (to != person1 && to == person2) {
+            return person1;
+        } else if (to == person1 && to != person2) {
+            return person2;
+        }
+
+        throw "No other person";
+    }
 };
+
+size_t CContact::nextUniqueId = 0;
 
 class CEFaceMask {
     vector<CContact> contacts;
+
+    void addPersonToList(CContact & contact, long unsigned person, vector<int> & list) {
+        try {
+            unsigned long otherPerson = contact.otherPerson(person);
+            if (find(list.begin(), list.end(), otherPerson) == list.end()) {
+                list.push_back(otherPerson);
+            }
+        }
+        catch (...) {}
+    }
+
+    vector<int> processContacts(long unsigned person, CTimeStamp fromT = CTimeStamp(0, 0, 0, 0, 0, 0), CTimeStamp toT = CTimeStamp(0, 0, 0, 0, 0, 0), bool withTimestamps = false) {
+        vector<int> listOfContacts;
+        for (auto& contact : contacts) {
+            if (withTimestamps) {
+                if (contact.containsPerson(person, fromT, toT)) {
+                    addPersonToList(contact, person, listOfContacts);
+                }
+            } else {
+                if (contact.containsPerson(person)) {
+                    addPersonToList(contact, person, listOfContacts);
+                }
+            }
+        }
+        return listOfContacts;
+    }
 
 public:
     CEFaceMask & addContact(const CContact & contact) {
         contacts.push_back(contact);
         return *this;
+    }
+
+    vector<int> listContacts(unsigned long person) {
+        return processContacts(person);
+    }
+
+    vector<int> listContacts(unsigned long person, CTimeStamp fromT, CTimeStamp toT) {
+        return processContacts(person, fromT, toT, true);
     }
 
 };
@@ -93,13 +192,12 @@ int main() {
     test.addContact(CContact(CTimeStamp(2021, 2, 21, 18, 0, 0), 123456789, 999888777));
     test.addContact(CContact(CTimeStamp(2021, 1, 5, 18, 0, 0), 123456789, 456456456));
     test.addContact(CContact(CTimeStamp(2021, 2, 1, 0, 0, 0), 123456789, 123456789));
-    /*
     assert(test.listContacts(123456789) == (vector<int>{999888777, 111222333, 456456456}));
     assert(test.listContacts(999888777) == (vector<int>{123456789, 555000222}));
     assert(test.listContacts(191919191) == (vector<int>{}));
     assert(test.listContacts(123456789, CTimeStamp(2021, 1, 5, 18, 0, 0), CTimeStamp(2021, 2, 21, 18, 0, 0)) == (vector<int>{999888777, 111222333, 456456456}));
     assert(test.listContacts(123456789, CTimeStamp(2021, 1, 5, 18, 0, 1), CTimeStamp(2021, 2, 21, 17, 59, 59)) == (vector<int>{999888777, 111222333}));
-    assert(test.listContacts(123456789, CTimeStamp(2021, 1, 10, 12, 41, 9), CTimeStamp(2021, 2, 21, 17, 59, 59)) == (vector<int>{111222333}));*/
+    assert(test.listContacts(123456789, CTimeStamp(2021, 1, 10, 12, 41, 9), CTimeStamp(2021, 2, 21, 17, 59, 59)) == (vector<int>{111222333}));
     return 0;
 }
 #endif /* __PROGTEST__ */
