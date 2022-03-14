90ceb626bfe5556627f3d6d87a529e8f03ac9229
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-03-14 21:52:10 +0100

    ex05: Implement class interface

--- tasks/exercise/ex05/cas.cpp
+++ tasks/exercise/ex05/cas.cpp
@@ -14,14 +14,145 @@ private:
     int m_Minute;
     int m_Second;
 
+    void recalculate() {
+
+        if (m_Second < 0) {
+            m_Second += 60 * 60 * 24;
+        }
+
+        m_Minute += m_Second / 60;
+        m_Hour += m_Minute / 60;
+
+        m_Second %= 60;
+        m_Minute %= 60;
+        m_Hour %= 24;
+    }
+
+    int toSeconds() const {
+        return ((m_Hour * 60) + m_Minute) * 60 + m_Second;
+    }
+
+    static int abs(int from) {
+        if (from > 0) {
+            return from;
+        }
+        return from * (-1);
+    }
+
 public:
     // constructor, destructor
+    CTime() : m_Hour(0), m_Minute(0), m_Second(0) { }
+
+    CTime(int hour, int minute, int second = 0) {
+        if (hour >= 24 || hour < 0
+            || minute >= 60 || minute < 0
+            || second >= 60 || second < 0) {
+            throw invalid_argument("Invalid values for time (hours must be 0-23, minutes must be 0-59, seconds must be 0-59)");
+        }
+
+        m_Hour = hour;
+        m_Minute = minute;
+        m_Second = second;
+    }
 
     // arithmetic operators
+    CTime & operator += (int seconds) {
+        m_Second += seconds;
+        recalculate();
+        return *this;
+    }
+
+    CTime operator + (int seconds) {
+        CTime newTime = *this;
+        newTime += seconds;
+        return newTime;
+    }
+
+    CTime & operator -= (int seconds) {
+        m_Second -= seconds;
+        recalculate();
+        return *this;
+    }
+
+    CTime operator - (int seconds) {
+        CTime newTime = *this;
+        newTime -= seconds;
+        return newTime;
+    }
+
+    friend int operator - (CTime & lhs, CTime & rhs) {
+        return abs(lhs.toSeconds() - rhs.toSeconds());
+    }
+
+    CTime & operator ++ () {
+        *this += 1;
+        return *this;
+    }
+
+    CTime & operator -- () {
+        *this -= 1;
+        return *this;
+    }
 
     // comparison operators
+    bool operator == (CTime & rhs) const {
+        return ((m_Hour == rhs.hours()) && (m_Minute == rhs.minutes()) && (m_Second == rhs.seconds()));
+    }
+
+    bool operator != (CTime & rhs) const {
+        return !(*this == rhs);
+    }
+
+    bool operator > (CTime & rhs) const {
+        if (m_Hour == rhs.hours()) {
+            if (m_Minute == rhs.minutes()) {
+                return m_Second > rhs.seconds();
+            }
+            return m_Minute > rhs.minutes();
+        }
+        return m_Hour > rhs.hours();
+    }
+
+    bool operator < (CTime & rhs) const {
+        if (m_Hour == rhs.hours()) {
+            if (m_Minute == rhs.minutes()) {
+                return m_Second < rhs.seconds();
+            }
+            return m_Minute < rhs.minutes();
+        }
+        return m_Hour < rhs.hours();
+    }
+
+    bool operator <= (CTime & rhs) const {
+        return (*this < rhs) || (*this == rhs);
+    }
+
+    bool operator >= (CTime & rhs) const {
+        return (*this > rhs) || (*this == rhs);
+    }
 
     // output operator
+    friend ostream & operator << (ostream & out, CTime & rhs) {
+        out << std::setfill(' ') << std::setw(2) << rhs.hours();
+        out << ":";
+        out << std::setfill('0') << std::setw(2) << rhs.minutes();
+        out << ":";
+        out << std::setfill('0') << std::setw(2) << rhs.seconds();
+        return out;
+    }
+
+    // helper functions
+    int hours() const {
+        return m_Hour;
+    }
+
+    int minutes() const {
+        return m_Minute;
+    }
+
+    int seconds() const {
+        return m_Second;
+    }
 
     friend class ::CTimeTester;
 };
