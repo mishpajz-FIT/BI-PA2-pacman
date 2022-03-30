fcc258467a524147fb5b8d6591a2f359351b84e1
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-03-30 21:56:51 +0200

    hw04: Document List using Doxygen

--- tasks/homework/hw04/soubor_s_verzemi.cpp
+++ tasks/homework/hw04/soubor_s_verzemi.cpp
@@ -8,27 +8,106 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
+/**
+ * @brief Data container
+ *
+ * Supports O(1) insertion and removal.
+ * Does not have fast random access.
+ *
+ * @tparam T Datatype
+ */
 template <typename T>
 class List {
+    // Implemented as doubly linked list
 
+
+    /**
+     * @brief List Node which stores element of List
+     *
+     * Linked list node.
+     *
+     */
     struct Node {
-        T value;
-        Node * next;
-        Node * prev;
+        T value; // Data
+        Node * next; // Next node
+        Node * prev; // Previous node
 
         Node(T newValue) : value(newValue), next(nullptr), prev(nullptr) { }
 
         Node(const Node & toCopy) : value(toCopy.value), next(nullptr), prev(nullptr) { }
     };
 
-    Node * head;
-    Node * tail;
+    Node * head; // First Node
+    Node * tail; // Last Node
 
-    size_t size;
+    size_t size; // Nodes in this list (updated manually by insertion/deletion)
+
+    /**
+     * @brief Insert element before Node
+     *
+     * If pointer to Node is nullptr, inserts at end.
+     *
+     * @param before Pointer to Node before which to insert element
+     * @param value Element which should be inserted
+     */
+    void insert(Node * before, const T & value) {
+        Node * newNode = new Node(value);
+
+        if (before == nullptr) { //If inserting at end, set new tail
+            newNode->prev = tail;
+            tail = newNode;
+        } else { //Set new node's prev from 'before' node's prev, set before's node prev as new node
+            newNode->prev = before->prev;
+            before->prev = newNode;
+        }
+
+        if (newNode->prev == nullptr) { //If at begin, set new hail
+            head = newNode;
+        } else { //Set next of new node's prev to new node
+            (newNode->prev)->next = newNode;
+        }
+        newNode->next = before; //Set next of new node to before
+
+        size++;
+    }
+
+    /**
+     * @brief Remove Node
+     *
+     * @param at Pointer to Node which should be removed
+     * @return T Element that was removed
+     */
+    T remove(Node * at) {
+        T value(at->value); // Store value that will be removed
+
+        if (at->prev != nullptr) {
+            at->prev->next = at->next; // Set next of previous Node
+        } else {
+            head = at->next; // Set new head if removing current head
+        }
+
+        if (at->next != nullptr) {
+            at->next->prev = at->prev; // Set previous of next Node
+        } else {
+            tail = at->prev; // Set new tail if removing current tail
+        }
+
+        delete at;
+        size--;
+
+        return value;
+    }
 
 public:
+    /**
+     * @brief Iterator for List
+     *
+     * Support forward/backward movement by one, element is accessed by dereferencing.
+     * Can also deduce if at end of the list, however, if at end can not be moved forward nor backward.
+     *
+     */
     struct ListIterator {
-        Node * ptr;
+        Node * ptr; // Pointer to Node which this Iterator represents
 
         ListIterator(const List & list, size_t position) : ptr(list.head) {
             for (size_t i = 0; i < position; i++) {
@@ -77,13 +156,13 @@ public:
 
     List(const List & copyFrom) : head(nullptr), tail(nullptr), size(copyFrom.size) {
         if (copyFrom.head != nullptr) {
-            head = new Node(*(copyFrom.head));
+            head = new Node(*(copyFrom.head)); // Copy head
             Node * iter = head;
             Node * iterCopy = copyFrom.head->next;
-            while (iterCopy != nullptr) {
-                iter->next = new Node(*(iterCopy));
-                iter->next->prev = iter;
-                iter = iter->next;
+            while (iterCopy != nullptr) { // Iteratively copy all nodes
+                iter->next = new Node(*(iterCopy)); // Set next and allocate new Node
+                iter->next->prev = iter; // Set prev
+                iter = iter->next; // Move iterators
                 iterCopy = iterCopy->next;
             }
             tail = iter;
@@ -92,7 +171,7 @@ public:
 
     ~List() {
         Node * iter = head;
-        while (iter != nullptr) {
+        while (iter != nullptr) { // Iteratively dealloc nodes
             Node * tmp = iter->next;
             delete iter;
             iter = tmp;
@@ -107,6 +186,7 @@ public:
             return *this;
         }
 
+        // Copy list and swap all values
         List copy(toAssign);
 
         size = copy.size;
@@ -122,29 +202,18 @@ public:
         return (*this);
     }
 
-    void insert(Node * before, const T & value) {
-        Node * newNode = new Node(value);
-
-        if (before == nullptr) { //If inserting at end, set new tail
-            newNode->prev = tail;
-            tail = newNode;
-        } else { //Set new node's prev from 'before' node's prev, set before's node prev as new node
-            newNode->prev = before->prev;
-            before->prev = newNode;
-        }
-
-        if (newNode->prev == nullptr) { //If at begin, set new hail
-            head = newNode;
-        } else { //Set next of new node's prev to new node
-            (newNode->prev)->next = newNode;
-        }
-        newNode->next = before; //Set next of new node to before
-
-        size++;
-    }
-
+    /**
+     * @brief Insert element before Iterator
+     *
+     * Moves Iterator to inserted element.
+     *
+     * @param before Iterator before which to insert element
+     * @param value Element which should be inserted
+     */
     void insert(ListIterator & before, const T & value) {
         insert(before.ptr, value);
+
+        // Move iterator to inserted Node
         if (before.ptr == nullptr) {
             before.ptr = tail;
         } else {
@@ -152,49 +221,61 @@ public:
         }
     }
 
-    T remove(Node * at) {
-        T value(at->value);
-
-        if (at->prev != nullptr) {
-            at->prev->next = at->next;
-        } else {
-            head = at->next;
-        }
-
-        if (at->next != nullptr) {
-            at->next->prev = at->prev;
-        } else {
-            tail = at->prev;
-        }
-
-        delete at;
-        size--;
-
-        return value;
-    }
-
+    /**
+     * @brief Remove element selected by Iterator
+     *
+     * Moves Iterator to next element after removed element.
+     *
+     * @param at Iterator which element should be removed
+     * @return T Element that was removed
+     */
     T remove(ListIterator & at) {
         ListIterator old = at;
         at++;
         return remove(old.ptr);
     }
 
+    /**
+     * @brief Insert element at beggining of List
+     *
+     * @param value Element which should be inserted
+     */
     void pushFront(const T & value) {
         insert(head, value);
     }
 
+    /**
+     * @brief Remove element at beggining of List
+     *
+     * @return T Element that was removed
+     */
     T popFront() {
         return remove(head);
     }
 
+    /**
+     * @brief Insert element at end of List
+     *
+     * @param value Element which should be inserted
+     */
     void pushBack(const T & value) {
         insert(nullptr, value);
     }
 
+    /**
+     * @brief Remove element at end of List
+     *
+     * @return T Element that was removed
+     */
     T popBack() {
         return remove(tail);
     }
 
+    /**
+     * @brief Get last element in list
+     *
+     * @return T& Reference to last element
+     */
     T & back() {
         return tail->value;
     }
@@ -203,6 +284,11 @@ public:
         return tail->value;
     }
 
+    /**
+     * @brief Get first element in list
+     *
+     * @return T& Reference to first element
+     */
     T & front() {
         return head->value;
     }
@@ -211,18 +297,41 @@ public:
         return head->value;
     }
 
+    /**
+     * @brief Get iterator to beggining of list
+     *
+     * @return ListIterator Iterator
+     */
     ListIterator begin() const {
         return ListIterator(head);
     }
 
+    /**
+     * @brief Get iterator to end of list
+     *
+     * Note this iterator points behind last element of list, can't be increased or decreased and does not point to any value.
+     *
+     * @return ListIterator Iterator
+     */
     ListIterator end() const {
         return ListIterator(nullptr);
     }
 
+    /**
+     * @brief
+     *
+     * @param at
+     * @return ListIterator
+     */
     ListIterator iterator(size_t at = 0) const {
         return ListIterator(*this, at);
     }
 
+    /**
+     * @brief Amount of elements in List
+     *
+     * @return size_t Amount of elements
+     */
     size_t getSize() const {
         return size;
     }
@@ -695,6 +804,15 @@ int main(void) {
     assert(f1.undoVersion());
     assert(readTest(f1, { 4, 70, 80 }, 20));
     assert(!f1.undoVersion());
+
+    CFile f002;
+    f002 = f001;
+    f002.seek(0);
+    f002.truncate();
+    assert(f001.fileSize() == 15);
+    assert(f002.fileSize() == 0);
+    f002.truncate();
+
     return EXIT_SUCCESS;
 }
 #endif /* __PROGTEST__ */
