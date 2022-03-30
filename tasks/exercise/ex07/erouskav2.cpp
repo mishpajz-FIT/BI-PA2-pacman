e0e31cf0eb40305d155942e40c868b43c54bb0e9
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-03-31 00:01:07 +0200

    ex07: Copy reusable classes from ex04

--- tasks/exercise/ex07/erouskav2.cpp
+++ tasks/exercise/ex07/erouskav2.cpp
@@ -18,17 +18,165 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
-class CTimeStamp {
+/**
+ * @brief Class for storing and comparing dates
+ *
+ * Values are not sanitized
+ */
+class CDate {
 public:
-  // constructor ( y, m, d, h, min, s )
-  // TODO
+    int year;
+    int month;
+    int day;
+
+    CDate(unsigned int y, unsigned int m, unsigned int d) : year(y), month(m), day(d) { }
+
+    bool isEqual(const CDate & otherDate) const {
+        return ((year == otherDate.year) && (month == otherDate.month) && (day == otherDate.day));
+    }
+
+    bool operator == (const CDate & rhs) const {
+        return isEqual(rhs);
+    }
+
+    bool operator > (const CDate & rhs) const {
+
+        if (year == rhs.year) {
+            if (month == rhs.month) {
+                if (day == rhs.day) {
+                    return false;
+                }
+                return day > rhs.day;
+            }
+            return month > rhs.month;
+        }
+        return year > rhs.year;
+    }
 };
 
-class CContact {
+/**
+ * @brief Class for stroring and comparing time
+ *
+ * Values are not sanitized
+ *
+ */
+class CTime {
 public:
-  // constructor ( timeStamp, phone1, phone2 )
-  // TODO
+    int hour;
+    int minute;
+    int second;
+
+    CTime(unsigned int h, unsigned int m, unsigned int s) : hour(h), minute(m), second(s) { }
+
+    bool isEqual(const CTime & otherTime) const {
+        return ((hour == otherTime.hour) && (minute == otherTime.minute) && (second == otherTime.second));
+    }
+
+    bool operator == (const CTime & rhs) const {
+        return isEqual(rhs);
+    }
+
+    bool operator > (const CTime & rhs) const {
+
+        if (hour == rhs.hour) {
+            if (minute == rhs.minute) {
+                if (second == rhs.second) {
+                    return false;
+                }
+                return second > rhs.second;
+            }
+            return minute > rhs.minute;
+        }
+        return hour > rhs.hour;
+    }
 };
+
+/**
+ * @brief Class for storing and comparing date and time
+ *
+ */
+class CTimeStamp {
+public:
+    CTime time;
+    CDate date;
+
+    CTimeStamp(unsigned int y, unsigned int mo, unsigned int d, unsigned int h, unsigned int mi, unsigned int s) : time(h, mi, s), date(y, mo, d) { }
+
+    bool isEqual(const CTimeStamp & otherTimestamp) const {
+        return ((time == otherTimestamp.time) && (date == otherTimestamp.date));
+    }
+
+    bool operator == (const CTimeStamp & rhs) const {
+        return isEqual(rhs);
+    }
+
+    bool operator >= (const CTimeStamp & rhs) const {
+
+        if (date == rhs.date) {
+            if (time == rhs.time) {
+                return true;
+            }
+            return time > rhs.time;
+        }
+        return date > rhs.date;
+    }
+};
+
+/**
+ * @brief Class representing contact between two persons, including date and time when this contact happend
+ *
+ */
+class CContact {
+    const CTimeStamp timestamp;
+    const unsigned long person1; // ID of person1
+    const unsigned long person2; // ID of person2
+
+public:
+    CContact(const CTimeStamp & ts, int p1, int p2) : timestamp(ts), person1(p1), person2(p2) { }
+
+    /**
+     * @brief Method to determine if contact contains person id
+     *
+     * @param p Person id to look for
+     * @return true Person is part of this contact
+     * @return false Person is not part of this contact
+     */
+    bool containsPerson(unsigned int p) const {
+        return ((person1 == p) || (person2 == p));
+    }
+
+    /**
+     * @brief Method to determine if contact contains person id and is between specified timestamps
+     *
+     * @param p Person id to look for
+     * @param fromT Timestamp to look from
+     * @param toT Timestamp to look to
+     * @return true Person is part of this contact
+     * @return false Person is not part of this contact
+     */
+    bool containsPerson(unsigned int p, const CTimeStamp & fromT, const CTimeStamp & toT) const {
+        return (containsPerson(p) && (timestamp >= fromT && toT >= timestamp));
+    }
+
+    /**
+     * @brief Method to get the second person of this contact
+     *
+     * Throws if contact does not contain searched person, or if contact contains only searched person .
+     *
+     * @param to Person in this contact
+     * @return unsigned long Person in this contact other than person in 'to'
+     */
+    unsigned long otherPerson(unsigned int to) const {
+        if (to != person1 && to == person2) {
+            return person1;
+        } else if (to == person1 && to != person2) {
+            return person2;
+        }
+
+        throw "No other person";
+    }
+};
+
 
 class CEFaceMask {
 public:
