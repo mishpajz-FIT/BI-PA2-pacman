52b40c9deefa18191c0cf38af9b5295fd3206bab
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-05-06 14:33:21 +0200

    semestral-implementation: Implement timer for timing events

---
+++ semestral/src/Utilities/Timer.cpp
@@ -0,0 +1,73 @@
+#include "Timer.h"
+
+// SECTION: TimerObject
+Timer::TimerObject::TimerObject(timepoint initTime, milliseconds perTime, std::function<void()> act, bool repeat) : initializeTime(initTime), periodTime(perTime), action(act), isRepeatingAction(repeat) {
+    if (perTime.count() == 0 && repeat) {
+        throw std::invalid_argument("Timer::TimerObject: TimerObject - repeating action with 0 period");
+    }
+}
+
+Timer::timepoint Timer::TimerObject::actionTime() const {
+    return (initializeTime + periodTime);
+}
+
+void Timer::TimerObject::callAction() {
+    action();
+}
+
+bool Timer::TimerObject::repeating() const {
+    return isRepeatingAction;
+}
+
+void Timer::TimerObject::updateBeginning(timepoint to) {
+    initializeTime = to;
+}
+
+bool Timer::TimerObject::operator < (const TimerObject & rhs) const {
+    return rhs.actionTime() < actionTime();
+}
+// !SECTION
+
+
+
+// SECTION: Timer
+Timer::timepoint Timer::adjustedTime() {
+    return (std::chrono::steady_clock::now() - timeOffset);
+}
+
+Timer::Timer() : paused(false), lastPausedTime(Timer::clock::now()), timeOffset(0) { }
+
+void Timer::togglePause() {
+    if (!paused) {
+        lastPausedTime = Timer::clock::now();
+    } else {
+        timeOffset += std::chrono::duration_cast<Timer::milliseconds>(Timer::clock::now() - lastPausedTime);
+    }
+
+    paused = !paused;
+}
+
+void Timer::update() {
+    if (paused) {
+        return;
+    }
+
+    while ((timerQueue.size() != 0) && (timerQueue.top().actionTime() <= adjustedTime())) {
+        Timer::TimerObject copy(timerQueue.top());
+        timerQueue.pop();
+
+        copy.callAction();
+
+        if (copy.repeating()) {
+            copy.updateBeginning(adjustedTime());
+            timerQueue.push(copy);
+        }
+    }
+}
+
+void Timer::addTrigger(unsigned int period, std::function<void()> action, bool repeating) {
+    TimerObject newObject(adjustedTime(), Timer::milliseconds(period), action, repeating);
+
+    timerQueue.push(newObject);
+}
+//!SECTION: Timer
---
+++ semestral/src/Utilities/Timer.h
@@ -0,0 +1,58 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <chrono>
+#include <functional>
+#include <queue>
+#include <deque>
+#include <stdexcept>
+
+class Timer {
+private:
+    typedef std::chrono::steady_clock clock;
+    typedef std::chrono::time_point<clock> timepoint;
+    typedef std::chrono::milliseconds milliseconds;
+
+    class TimerObject {
+    private:
+        timepoint initializeTime;
+        milliseconds periodTime;
+
+        std::function<void()> action;
+        bool isRepeatingAction;
+
+    public:
+
+        TimerObject(timepoint initTime, milliseconds perTime, std::function<void()> act, bool repeat);
+
+        timepoint actionTime() const;
+
+        void callAction();
+
+        bool repeating() const;
+
+        void updateBeginning(timepoint to);
+
+        bool operator < (const TimerObject & rhs) const;
+    };
+
+private:
+    bool paused;
+    timepoint lastPausedTime;
+    milliseconds timeOffset;
+
+    std::priority_queue<TimerObject> timerQueue;
+
+    timepoint adjustedTime();
+
+public:
+    Timer();
+
+    void togglePause();
+
+    void update();
+
+    void addTrigger(unsigned int milliseconds, std::function<void()> action, bool repeating = false);
+
+};
+#endif /* TIMER_H */
